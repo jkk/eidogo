d169e8359c8c9aeb41e59bde4069ca4c531b17b7
jkkramer <jkkramer@3a017fa3-8d26-0410-b3dd-4f2ae428da9e>
2007-09-15 21:24:36 +0000

    miscellaneous tweaks
    git-svn-id: https://eidogo.googlecode.com/svn/trunk@51 3a017fa3-8d26-0410-b3dd-4f2ae428da9e

--- php/content_header.php
+++ php/content_header.php
@@ -9,4 +9,12 @@ if ($extension == "css") {
 if ($extension == "js") {
 	header("Content-type: text/javascript");
 }
+
+if ($extension == "js" || $extension == "css") {
+    //header("Content-length: " . filesize();
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s",mktime (0,0,0,1,1,2000)) . " GMT"); 
+    header("Expires: Mon, 26 Jul 2040 05:00:00 GMT"); 
+    header("Cache-Control: max-age=10000000, s-maxage=1000000, proxy-revalidate, must-revalidate, public");
+}
+
 ?>
