e3ccb6086ab1c20eda547e42a6c55583038442af
jkkramer <jkkramer@3a017fa3-8d26-0410-b3dd-4f2ae428da9e>
2007-11-15 09:26:06 +0000

    renamed build script, updated release.sh
    git-svn-id: https://eidogo.googlecode.com/svn/trunk@66 3a017fa3-8d26-0410-b3dd-4f2ae428da9e

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ DEST="releases/$1"
 mkdir -p $DEST/sgf
 cp -r player $DEST
 find $DEST -name .svn | xargs rm -rf
-find $DEST -name .psd | xargs rm -rf
+find $DEST -name '*.psd' | xargs rm -rf
 rm $DEST/player/custom_rhino.jar
 cp sgf/example.sgf $DEST/sgf
 cp sgf/test.sgf $DEST/sgf
