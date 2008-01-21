402b8779ae98479aa7314c79e90b48ba0fdc1eb6
jkkramer <jkkramer@3a017fa3-8d26-0410-b3dd-4f2ae428da9e>
2008-01-21 20:13:55 +0000

    changelog, updated release script, ready for 1.1
    git-svn-id: https://eidogo.googlecode.com/svn/trunk@97 3a017fa3-8d26-0410-b3dd-4f2ae428da9e

--- release.sh
+++ release.sh
@@ -10,5 +10,6 @@ rm $DEST/player/custom_rhino.jar
 cp sgf/blood_vomit.sgf $DEST/sgf
 cp sgf/example.sgf $DEST/sgf
 cp sgf/test.sgf $DEST/sgf
-cp example.html $DEST
-cp example2.html $DEST
+cp sgf/problem.sgf $DEST/sgf
+cp sgf/problem2.sgf $DEST/sgf
+cp example*.html $DEST
